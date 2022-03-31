40042cfb36523d9e993278373712eb650d18137c
Ibrahim Boubakri <ibrahim.boubakri@ensi-uma.tn>
2022-03-31 22:00:55 +0100

    adminActivities
    -addUser
    -addTeam
    -User class
    -java + xml

---
+++ app/src/main/java/com/example/loginacessapp/User.java
@@ -0,0 +1,17 @@
+package com.example.loginacessapp;
+
+public class User {
+
+
+    public String username,as,password;
+
+    public User(){
+
+    }
+
+    public User(String username , String as ,String password){
+        this.username = username;
+        this.as = as;
+        this.password = password;
+    }
+}
